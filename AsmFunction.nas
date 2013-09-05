16d48eb0c3e9efaf6c7026b47230fe668cab8e13
mamk <mamiaokui@gmail.com>
2013-09-06 01:23:55 +0800

    add palette, but not working

--- AsmFunction.nas
+++ AsmFunction.nas
@@ -1,15 +1,18 @@
 ; naskfunc
 ; TAB=4
-
 [BITS 32]
 
 		GLOBAL	hlt	
         GLOBAL  writeMemory8
+        GLOBAL  cli
+        GLOBAL  out8
+        GLOBAL  loadEflags
+        GLOBAL  storeEflags
+
 
 
 [SECTION .text]
 hlt:	; void hlt(void);
-
 		HLT
 		RET
 
@@ -19,3 +22,24 @@ writeMemory8: ;void writeMemory8(int, int);
     mov al,  [esp + 8]
     mov [ecx], al
     ret
+
+cli: ;void cli(void);
+		CLI
+		RET
+
+out8: ;void out8(int port, int data);
+		MOV		EDX,[ESP+4]		; port
+		MOV		AL,[ESP+8]		; data
+		OUT		DX,AL
+		RET
+
+loadEflags: ;int loadEflags(void);
+		PUSHFD		; PUSH EFLAGS INTO STACK
+		POP		EAX
+		RET
+
+storeEflags: ;void storeEflags(int eflags);
+		MOV		EAX,[ESP+4]
+		PUSH	EAX
+		POPFD		; POP EAX TO EFLAGS
+		RET
--- BootProgram.c
+++ BootProgram.c
@@ -6,19 +6,62 @@ extern int loadEflags(void);
 extern void storeEflags(int eflags);
 
 
-void init_palette(void);
-void set_palette(int start, int end, unsigned char *rgb);
+void initPalette(void);
+void setPalette(int start, int end, unsigned char *rgb);
 
 
 void MKOSMain(void)
 {
     int i;
-    int j;
-    for (j = 0; j < 100; j++) {
-        for (i = 0xa0000; i <= 0xaffff; i++) {
-            writeMemory8(i, j);
-        }
+	//initPalette(); //not working, don't know the reason.
+
+    char *p = (char *)0xa0000;
+
+    for (i = 0; i <= 0xffff; i++) {
+        p[i] = i&0xf;
     }
+
     while (1)
         hlt();
+}
+
+
+void initPalette(void)
+{
+	static unsigned char table_rgb[16 * 3] = {
+		0x00, 0x00, 0x00, // black
+		0xff, 0x00, 0x00, // bright red
+		0x00, 0xff, 0x00, // bright green
+		0xff, 0xff, 0x00, // bright yellow
+		0x00, 0x00, 0xff, // bright blue
+		0xff, 0x00, 0xff, // bright purple
+		0x00, 0xff, 0xff, // light blue
+		0xff, 0xff, 0xff, // white
+		0xc6, 0xc6, 0xc6, // bright gray
+		0x84, 0x00, 0x00, // dark red
+		0x00, 0x84, 0x00, // dark green
+		0x84, 0x84, 0x00, // dark yellow
+		0x00, 0x00, 0x84, // dark blue
+		0x84, 0x00, 0x84, // dark purple
+		0x00, 0x84, 0x84, // light dark blue
+		0x84, 0x84, 0x84, // dark gray
+	};
+    	setPalette(0, 15, table_rgb);
+	return;
+}
+
+void setPalette(int start, int end, unsigned char *rgb)
+{
+	int i, eflags;
+	eflags = loadEflags();
+	cli(); 					
+        out8(0x03c8, start);	
+	for (i = start; i <= end; i++) {
+		out8(0x03c9, rgb[0]/4);
+		out8(0x03c9, rgb[1]/4);
+		out8(0x03c9, rgb[2]/4);
+		rgb += 3;
+	}
+	storeEflags(eflags);
+	return;
 }
