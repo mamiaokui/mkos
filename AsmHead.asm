fa5ddceb894ba28fcfea7d9db125f1216cf2868f
mamk <mamiaokui@gmail.com>
2013-09-09 02:07:02 +0800

    fix to do. refactor variable, delete unused code

--- AsmHead.asm
+++ AsmHead.asm
@@ -6,22 +6,22 @@
 	db	(%1 >> 24) & 0FFh			                    ; 段基址 3				(1 字节)
 %endmacro ; 共 8 字节
 
-DA_DRW		EQU	92h	; 存在的可读写数据段属性值
-DA_DRWA		EQU	93h	; 存在的已访问可读写数据段类型值
-DA_C		EQU	98h	; 存在的只执行代码段属性值
-DA_32		EQU	4000h	; 32 位段
+DA_DRW		equ	92h	    ; 存在的可读写数据段属性值
+DA_DRWA		equ	93h	    ; 存在的已访问可读写数据段类型值
+DA_C		equ	98h	    ; 存在的只执行代码段属性值
+DA_32		equ	4000h	; 32 位段
 
 
-VMODE	EQU		0x0ff0		
-SCRNX	EQU		0x0ff4		
-SCRNY	EQU		0x0ff8		
-VRAM	EQU		0x0ffc
+VMODE	equ		0x0ff0		
+SCRNX	equ		0x0ff4		
+SCRNY	equ		0x0ff8		
+VRAM	equ		0x0ffc
 
 
-IPLPOS	EQU		0x00100000	;for IPL
-ASMHEADPOS	EQU		0x00008200 ;for AsmHead
-BOOTPROGRAM	EQU		0x00280000	;for BootProgram
-FONTDATA	EQU		0x0010000	;for FONTDATA
+IPL_POS	    equ		0x00100000	;for IPL
+ASMHEAD_POS	equ		0x00008200  ;for AsmHead
+BOOTPROGRAM_POS	equ		0x00280000	;for BootProgram
+FONTDATA_POS	equ		0x0010000	;for FONTDATA_POS
         
 
 
@@ -30,8 +30,8 @@ FONTDATA	EQU		0x0010000	;for FONTDATA
 org	0x8200
 	jmp	LABEL_BEGIN
 
-        [SECTION .gdt]
-; GDT
+[SECTION .gdt]
+
 ;                                  段基址,            段界限,              属性
 LABEL_GDT:	    	Descriptor	       0,                0,                0   	; 空描述符
 LABEL_DESC_CODE32:	Descriptor	       0,       0x10000000,     DA_C + DA_32	; 非一致代码段, 32  段界限为数据段地址跨度减1
@@ -43,64 +43,64 @@ GdtPtr		dw	GdtLen - 1	    ; GDT界限
 		dd	0		            ; GDT基地址
 
 ; GDT 选择子
-SelectorCode32		equ	LABEL_DESC_CODE32	- LABEL_GDT    ;代码段选择子
+SELECTOR_CODE_32		equ	LABEL_DESC_CODE32	- LABEL_GDT    ;代码段选择子
 SelectorVideo		equ	LABEL_DESC_VIDEO	- LABEL_GDT    ;数据段选择子
 ; END of [SECTION .gdt]
 
 [SECTION .s16]
 [BITS	16]
-memcpy:
-		MOV		EAX,[ESI]
-		ADD		ESI,4
-		MOV		[EDI],EAX
-		ADD		EDI,4
-		SUB		ECX,1
-		JNZ		memcpy		
-		RET
+memcpy16:
+		mov		eax,[esi]
+		inc		esi
+		mov		[edi],eax
+		inc		edi
+		sub		ecx,1
+		JNZ		memcpy16		
+		ret
 
 LABEL_BEGIN:
-		MOV		AL,0x13	
-		MOV		AH,0x00
+		mov		AL,0x13	
+		mov		AH,0x00
 		INT		0x10
-		MOV		BYTE [VMODE],8
-		MOV		WORD [SCRNX],320
-		MOV		WORD [SCRNY],200
-		MOV		DWORD [VRAM],0x000a0000
+		mov		BYTE [VMODE],8
+		mov		WORD [SCRNX],320
+		mov		WORD [SCRNY],200
+		mov		DWORD [VRAM],0x000a0000
 
-		MOV		AL,0xff
+		mov		AL,0xff
 		OUT		0x21,AL
 		NOP					
 		OUT		0xa1,AL
 
 ; for ipl
 
-		MOV		ESI,0x7c00	
-		MOV		EDI,IPLPOS	
-		MOV		ECX,512/4
-		CALL	memcpy
+		mov		esi,0x7c00	
+		mov		edi,IPL_POS	
+		mov		ecx,512
+		CALL	memcpy16
 
 
 ; for asmhead
-		MOV		ESI,ASMHEADPOS
-		MOV		EDI,IPLPOS+512
-		MOV		ECX,bootprogram-0x8200
-		CALL	memcpy
+		mov		esi,ASMHEAD_POS
+		mov		edi,IPL_POS+512
+		mov		ecx,(bootprogram-0x8200)
+		CALL	memcpy16
 
 ;;; for font
-   		MOV		EBX, bootprogram
-        MOV     ESI, EBX
-        MOV     EDI, FONTDATA
-        MOV     ECX, 3592/4
-        CALL    memcpy
+   		mov		ebx, bootprogram
+        mov     esi, ebx
+        mov     edi, FONTDATA_POS
+        mov     ecx, 3592
+        CALL    memcpy16
 
 
 ; for bootprogram
 
-		MOV		EBX, bootprogram + 3952
-        MOV     ESI, EBX
-        MOV     EDI, BOOTPROGRAM
-        MOV     ECX, 1024*1024*5/4
-        CALL    memcpy
+		mov		ebx, bootprogram + 3952
+        mov     esi, ebx
+        mov     edi, BOOTPROGRAM_POS
+        mov     ecx, 1024*1024*5
+        CALL    memcpy16
 
 
 	mov	ax, cs
@@ -145,7 +145,7 @@ LABEL_BEGIN:
 	mov	cr0, eax                                   ;对CRO寄存器的第一位置1，表示容许保护模式开启。
 
 	; 真正进入保护模式
-	jmp	dword SelectorCode32:0	; 执行这一句会把 SelectorCode32 装入 cs, 并跳转到 Code32Selector:0  处
+	jmp	dword SELECTOR_CODE_32:0	    ; 执行这一句会把 SELECTOR_CODE_32 装入 cs, 并跳转到 Code32Selector:0  处
                                         ;将代码段的选择子装入cs
 
 [BITS	32]
@@ -161,73 +161,55 @@ LABEL_SEG_CODE32:
     mov esp, StackTop
     call RellocELF
 
-	jmp	BOOTPROGRAM
+	jmp	BOOTPROGRAM_POS
 
-; 遍历每一个 Program Header，根据 Program Header 中的信息来确定把什么放进内存，放到什么位置，以及放多少。however, something wrong when copy all the program header info, so I only copy the first one.
+; 遍历每一个 Program Header，根据 Program Header 中的信息来确定把什么放进内存，放到什么位置，以及放多少。
 RellocELF:	
 	xor	esi, esi
-	mov	cx, word [BOOTPROGRAM + 2Ch]    ;  ecx <- pELFHdr->e_phnum
+	mov	cx, word [BOOTPROGRAM_POS + 2Ch]    ;  ecx <- pELFHdr->e_phnum
+    ;; dec cs because the last one like GNU_STACK      0x000000 0x00000000 0x00000000 0x00000 0x00000 RWE 0x4 cause copy problem.
+    dec cx
 	movzx	ecx, cx					
-	mov	esi, [BOOTPROGRAM + 1Ch]	    ; esi <- pELFHdr->e_phoff
-	add	esi, BOOTPROGRAM		        ; esi <- OffsetOfKernel + pELFHdr->e_phoff
+	mov	esi, [BOOTPROGRAM_POS + 1Ch]	    ; esi <- pELFHdr->e_phoff
+	add	esi, BOOTPROGRAM_POS		        ; esi <- OffsetOfKernel + pELFHdr->e_phoff
 .Begin:
 	mov	eax, [esi + 0]
 	cmp	eax, 0				            ; PT_NULL
 	jz	.NoAction
 	push	dword [esi + 010h]		    ; size	
    	mov	eax, [esi + 04h]	
-	add	eax, BOOTPROGRAM	            ; memcpy(	(void*)(pPHdr->p_vaddr),
+	add	eax, BOOTPROGRAM_POS	        ; memcpy(	(void*)(pPHdr->p_vaddr),
 	push	eax				            ; src		uchCode + pPHdr->p_offset,
 	push	dword [esi + 08h]	        ; dst			pPHdr->p_filesz;
         
-	call	MemCpy			
+	call	memcpy32
 
 	add	esp, 12			
 .NoAction:
 	add	esi, 020h			            ; esi += pELFHdr->e_phentsize
 	dec	ecx
-    ;;          	jnz	.Begin        ;;fixme: delete this code is wrong.
-	ret
-; ------------------------------------------------------------------------
-; 内存拷贝，仿 memcpy
-; ------------------------------------------------------------------------
-; void* MemCpy(void* es:pDest, void* ds:pSrc, int iSize);
-; ------------------------------------------------------------------------
-MemCpy:
-	push	ebp
-	mov	ebp, esp
 
-	push	esi
+    jnz	.Begin     
+	ret
+
+memcpy32:
+   	push	esi
 	push	edi
 	push	ecx
-
-	mov	edi, [esp + 20]	; Destination
-	mov	esi, [esp + 24]	; Source
-	mov	ecx, [esp + 28]	; Counter
-
-
-.1:
-	cmp	ecx, 0	
-	jz	.2		
-
-	mov	al, [ds:esi]
-	inc	esi			
-					
-	mov	byte [es:edi], al
-	inc	edi		
-
-	dec	ecx	
-	jmp	.1	
-.2:
-	mov	eax, [ebp + 8]
-
-	pop	ecx
+    mov ecx, [esp + 24]
+    mov esi, [esp + 20]
+    mov edi, [esp + 16]
+copying:        
+    mov		eax,[esi]
+    inc		esi
+    mov		[edi],eax
+    inc		edi
+    sub		ecx,1
+    JNZ		copying
+cpyend:
+   	pop	ecx
 	pop	edi
 	pop	esi
-	mov	esp, ebp
-	pop	ebp
+    ret
 
-	ret			
-
-SegCode32Len	equ	$ - LABEL_SEG_CODE32 
 bootprogram:
